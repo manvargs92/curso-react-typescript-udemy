de49e5602157736141d6516a8cf9a9d87bb6ba7d
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-22 10:27:04 -0600

    12 - Recorriendo arrays con for.

---
+++ IntroduccionJS/js/12-recorriedo-arrays-for.js
@@ -0,0 +1,8 @@
+
+/* Recorriendo arrays con for */
+const tecnologias = ["HTML", "CSS", "JavaScript", "React.js", "Node.js"];
+
+for (let i = 0; i < tecnologias.length; i++) {
+    console.log(tecnologias[i]);
+}
+
