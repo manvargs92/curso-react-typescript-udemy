93433d7c67c97138096541323a164de9534a52ad
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-22 08:46:50 -0600

    09 - Arrays o arreglos.

---
+++ IntroduccionJS/js/09-arrays.js
@@ -0,0 +1,9 @@
+
+/* Arrays o arreglos */
+const tecnologias = [20,30,40,true,"React.js"];
+
+console.log(tecnologias);
+console.table(tecnologias);
+
+// acceder a los valores del arreglo
+console.log(tecnologias[1]);
