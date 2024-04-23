d0a719b3b076bbb272c101f769db41d72271579f
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-23 11:15:50 -0600

    20 - Comparador estricto.

---
+++ IntroduccionJS/js/20-comparador-estricto.js
@@ -0,0 +1,24 @@
+
+/* Comparando con comparador estricto */
+const numero1 = 20;
+const numero2 = "20";
+
+/**
+    == comparador no estricto
+    === comparador estricto
+*/
+
+console.log(typeof(numero1));
+console.log(typeof(numero2));
+
+if (numero1 == numero2) {
+    console.log("Sí son iguales");
+} else {
+    console.log("No son iguales");
+}
+
+if (numero1 === numero2) {
+    console.log("Sí son iguales");
+} else {
+    console.log("No son iguales");
+}
