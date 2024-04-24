b37f91124d7fac65c31f0aca16a73bc3dc662cf2
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-24 17:14:48 -0600

    29 - DOM - seleccionar elementos.

---
+++ IntroduccionJS/js/29-dom-seleccionar-elementos.js
@@ -0,0 +1,14 @@
+
+/* DOM */
+// Selectores
+const heading = document.querySelector(".heading"); // retorna desde nada hasta 1 elemento
+
+console.log(heading);
+console.log(heading.tagName);
+console.log(heading.textContent);
+console.log(heading.classList);
+console.log(heading.id);
+
+const enlaces = document.querySelectorAll(".navegacion a"); // retorna todos los elementos que tengan el selector
+
+console.log(enlaces);
