c98a0ad7aa9944b850038bbc41440450249faf19
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-25 12:14:18 -0600

    Cómo trabajar con JSX.

--- guitarla/src/components/Header.jsx
+++ guitarla/src/components/Header.jsx
@@ -1,10 +1,43 @@
+import { Fragment } from "react" // usar <Fragment> para no tener divs innecesarios como etiqueta padre
+import React from "react"; // usar <React.Fragment> para no tener divs innecesarios como etiqueta padre
 
 // export default function Header() { // exportar componente en una sola línea
 function Header() {
     
+    const total = 100;
+
     return (
-        <h1>Desde Header</h1>
+        <Fragment>
+            <p>Total a pagar: {total}</p>
+            <p>Después</p>
+        </Fragment>
     );
 }
+
+/* Importando React from react */
+// function Header() {
+    
+//     const total = 100;
+
+//     return (
+//         <React.Fragment>
+//             <p>Total a pagar: {total}</p>
+//             <p>Después</p>
+//         </React.Fragment>
+//     );
+// }
+
+/* Otra forma pero sin importar nada */
+// function Header() {
+    
+//     const total = 100;
+
+//     return (
+//         <>
+//             <p>Total a pagar: {total}</p>
+//             <p>Después</p>
+//         </>
+//     );
+// }
 
 export default Header;
