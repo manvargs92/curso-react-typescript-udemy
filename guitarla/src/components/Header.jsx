80d958363b49168766256cedf19c033c2646b8d1
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-25 11:55:01 -0600

    Exportar, importar y usar un componente en ReactJS.

---
+++ guitarla/src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+// export default function Header() { // exportar componente en una sola línea
+function Header() {
+    
+    return (
+        <h1>Desde Header</h1>
+    );
+}
+
+export default Header;
