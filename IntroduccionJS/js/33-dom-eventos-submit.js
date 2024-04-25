e382b690ec24cf809e46c85bdba48207b1c7f9ab
manvargs92 <manuel.vargas.garrido@outlook.com>
2024-04-24 19:06:01 -0600

    33 - DOM - validando formularios y mostrando alertas.

--- IntroduccionJS/js/33-dom-eventos-submit.js
+++ IntroduccionJS/js/33-dom-eventos-submit.js
@@ -7,14 +7,35 @@ formulario.addEventListener("submit", (e) => {
     const nombre = document.querySelector("#nombre");
     const password = document.querySelector("#password");
 
+    // prevenir nuevas alertas
+    const alertaPrevia = document.querySelector(".alerta");
+    // if (alertaPrevia) {
+    //     alertaPrevia.remove();
+    // }
+    alertaPrevia?.remove(); // con optional chaining
+
+    const alerta = document.createElement("DIV");
+    alerta.classList.add("alerta", "text-white", "uppercase", "text-sm", "text-center", "p-2", "font-black");
+
     if (nombre.value == "" || password.value == "") {
         console.log("Todos los campos son obligatorios");
+        alerta.textContent = "Todos los campos son obligatorios";
+        alerta.classList.add("bg-red-500");
     } else {
         console.log("Enviando formulario...");
         console.log("Iniciando sesión...");
+        alerta.textContent = "Iniciando sesión...";
+        alerta.classList.add("bg-green-500");
     }
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
 
     console.log(nombre.value);
     console.log(password.value);
+
+    console.log(alerta);
+
+    formulario.appendChild(alerta);
 
 });
